b822075874918f79371145334c695549f1b63dec
trungdc-ha <dangchitrungha@gmail.com>
2023-05-11 06:39:11 +0700

    TrungDC update source app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <h1>hello</h1>
-  );
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        axios.get("https://my-json-server.typicode.com/trungdc-ha/reactjs-ci/posts")
+            .then((res) => {
+                setPost(res.data)
+            })
+    }, [])
+
+    return (
+        <>
+            <h1>{post.id}</h1>
+            <h1>{post.title}</h1>
+            <h1>{post.author}</h1>
+        </>
+    );
 }
 
 export default App;
